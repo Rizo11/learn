4397045e0be1ab0b082844b08df5969ace6cf3a4
Mukhammadrizo Maribjonov <M.Maribjonov@itechart-group.com>
2022-05-07 11:00:13 +0500

    finline

--- FInline/Program.cs
+++ FInline/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+namespace FInline
+{
+    public class Program
+    {
+        const int _max = 1000000;
+        public static void Main(string[] args)
+        {
+            Method1();
+            Method2();
+
+            var timer = new Stopwatch();
+            int sum = 0;
+            timer.Start();
+            for (var i = 0; i < _max; i++)
+            {
+                sum += Method1();
+            }
+            timer.Stop();
+            System.Console.WriteLine($"Method1: {timer.ElapsedTicks}");
+            timer.Reset();
+            sum = 0;
+            timer.Start();
+            for (var i = 0; i < _max; i++)
+            {
+                sum += Method2();
+            }
+            timer.Stop();
+            System.Console.WriteLine($"Method2: {timer.ElapsedTicks}");
+            timer.Reset();
+        }
+        static int Method1()
+        {
+            return 1;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        static int Method2()
+        {
+            return 1;
+        }
+    }
+
+}
