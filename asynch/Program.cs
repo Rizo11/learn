cd3843d6f373916fcdb78627fa4ee697d1cbd4a1
Rizo11 <maribjonov2002@gmail.com>
2022-05-09 23:08:21 +0500

    asunchITDN

---
+++ asynch/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace asynch;
+
+class Programm
+{
+    static async Task Main(string[] args)
+    {
+        System.Console.WriteLine(await MakeTeaAsync());
+    }
+
+    // public static string MakeTea()
+    // {
+    //     var water = BoilWater();
+
+    //     System.Console.WriteLine("take the cups out");
+    //     System.Console.WriteLine("put the tea in the cup");
+    //     var tea = $"pour {water} in the cup";
+
+    //     return tea;
+
+    // }
+
+    // public static string BoilWater()
+    // {
+    //     System.Console.WriteLine("start the kettle");
+    //     System.Console.WriteLine("wait for the kettle to boil");
+    //     Task.Delay(1000).GetAwaiter().GetResult();
+    //     System.Console.WriteLine("water boiled");
+    //     return "water";
+    // }
+
+    public static async Task<string> MakeTeaAsync()
+    {
+        var boilingWater = BoilWaterAsync();
+
+        System.Console.WriteLine("take the cups out");
+        System.Console.WriteLine("put the tea in the cup");
+
+        var water = await boilingWater;
+        var tea = $"pour {water} in the cup";
+
+        return tea;
+
+    }
+
+    public static async Task<string> BoilWaterAsync()
+    {
+        System.Console.WriteLine("start the kettle");
+
+        System.Console.WriteLine("wait for the kettle to boil");
+        await Task.Delay(1000);
+
+        System.Console.WriteLine("water boiled");
+        return "water";
+    }
+}
---
+++ asynchITDN/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace asynchITDN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //use this with MyClass1
+            // System.Console.WriteLine("Main ThreadID = {0}\n", Thread.CurrentThread.ManagedThreadId);
+            // MyClass1 my = new();
+            // my.OperationAsync();
+            // Console.ReadKey();
+        
+            // use this with MyClass2
+            MyClass2 my = new();
+            my.OperationAsync();
+
+            System.Console.WriteLine("Main ThreadID ended it's job = {0}\n", Thread.CurrentThread.ManagedThreadId);
+            
+            Console.ReadKey();
+        }
+    } 
+
+    class MyClass2
+    {
+        int Operation()
+        {
+            Console.WriteLine("Operation is executed in ThreadId: {0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            return 2 + 2;
+        }
+
+
+        // async refers that a method is asynchronous 
+        public async void OperationAsync()
+        {
+            // without async
+            // Task<int> task = Task<int>.Factory.StartNew(Operation);
+            // TaskAwaiter<int> awaiter = task.GetAwaiter();
+            // Action continuation = () => System.Console.WriteLine("\nResult: {0}\n", awaiter.GetResult());
+            // awaiter.OnCompleted(continuation);
+
+
+            // with async
+            // await is for waiting for the task to complete, waiting for async task to complete
+
+            // Main Thread
+            Task<int> task = Task<int>.Factory.StartNew(Operation);
+
+            // Another thread
+            System.Console.WriteLine("\nResult: {0}\n", await task);
+        }
+    }
+    class MyClass1
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Operation ThreadId: {0}", Thread.CurrentThread.ManagedThreadId);
+            System.Console.WriteLine("Begin");
+            Thread.Sleep(2000);
+            System.Console.WriteLine("End\n");
+        }
+
+        public async void OperationAsync()
+        {
+            // Thread's id is same as the main thread's id
+            // this methods starts on the same thread as the main thread
+            Console.WriteLine("OperationAsync ( Part 1 ) ThreadId: {0}\n", Thread.CurrentThread.ManagedThreadId);
+            
+            Task task = new Task(Operation);
+            task.Start();
+            await task;
+
+            // Thread's id is same as the 2nd thread's id
+            // this methods ends on the same thread as the 2nd thread
+            Console.WriteLine("OperationAsync ( Part 2 ) ThreadId: {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
