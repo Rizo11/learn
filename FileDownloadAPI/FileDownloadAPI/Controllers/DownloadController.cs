d48023ddc26f4c0a68015aa70aa7ebcb6ed2ef03
Mukhammadrizo Maribjonov <M.Maribjonov@itechart-group.com>
2022-05-17 18:11:31 +0500

    file download api

--- FileDownloadAPI/FileDownloadAPI/Controllers/DownloadController.cs
+++ FileDownloadAPI/FileDownloadAPI/Controllers/DownloadController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,20 +16,70 @@ namespace FileDownloadAPI.Controllers
         }
 
         // GET: api/<DownloadController>
+        //[HttpGet]
+        //public FileContentResult Get()
+        //{
+        //    //1st method
+        //    //var data = System.IO.File.ReadAllBytes(filePath);
+        //    //var result = new FileContentResult(data, "application/octet-stream")
+        //    //{
+        //    //    FileDownloadName = "File.csv"
+        //    //};
+
+        //    //return result;
+
+        //    // 2nd method
+        //    return File(System.IO.File.ReadAllBytes(filePath), "application/octet-stream", "File.csv");
+        //}
+
+        /// <summary>
+        /// returens a .xlsx
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public FileContentResult Get()
+        public FileContentResult GetXlsx()
         {
-            // 1st method
-            //var data = System.IO.File.ReadAllBytes(filePath);
-            //var result = new FileContentResult(data, "application/octet-stream")
-            //{
-            //    FileDownloadName = "File.csv"
-            //};
+            MemoryStream content = new MemoryStream();
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Employee");
+                //First add the headers
+                worksheet.Cells[1, 1].Value = "ID";
+                worksheet.Cells[1, 2].Value = "Name";
+                worksheet.Cells[1, 3].Value = "Gender";
+                worksheet.Cells[1, 4].Value = "Salary (in $)";
 
-            //return result;
+                //Add values
+                worksheet.Cells["A2"].Value = 1000;
+                worksheet.Cells["B2"].Value = "Jon";
+                worksheet.Cells["C2"].Value = "M";
+                worksheet.Cells["D2"].Value = 5000;
 
-            // 2nd method
-            return File(System.IO.File.ReadAllBytes(filePath), "application/octet-stream", "File.csv");
+                worksheet.Cells["A3"].Value = 1001;
+                worksheet.Cells["B3"].Value = "Graham";
+                worksheet.Cells["C3"].Value = "M";
+                worksheet.Cells["D3"].Value = 10000;
+
+                worksheet.Cells["A4"].Value = 1002;
+                worksheet.Cells["B4"].Value = "Jenny";
+                worksheet.Cells["C4"].Value = "F";
+                worksheet.Cells["D4"].Value = 5000;
+                byte[] bytesXlsx = package.GetAsByteArray();
+
+                //return new FileStreamResult(content, "application/octet-stream")
+                //{
+                //    FileDownloadName = "test.xlsx"
+                //};
+
+                //return File(bytesXlsx, "application/octet-stream", "File.xlsx");
+
+                var result = new FileContentResult(bytesXlsx, "application/octet-stream")
+                {
+                    FileDownloadName = "File.csv"
+                };
+
+                return result;
+            }
         }
     }
 }
