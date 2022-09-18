e651817fa30b4138a6ae9bd4256a797f408f42f5
Rizo11 <maribjonov2002@gmail.com>
2022-09-18 06:15:45 +0300

    unit testing

---
+++ ExceptionsLibrary/DemoCode.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsLibrary
+{
+    public class DemoCode
+    {
+        public int GrandParent(int position)
+        {
+            int output = 0;
+            Console.WriteLine("Opening Db connection");
+
+            try
+            {
+                output = Parent(position);
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("wrong info " + ex);
+            }
+            catch(Exception ex)
+            {
+                //Do some logging
+                throw new Exception("You passed in bad data", ex);
+            }
+            finally
+            {
+                Console.WriteLine($"Closing Db connection");
+            }
+
+            return output;
+        }
+
+        public int Parent(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int result = 0;
+            //try
+            //{
+                int[] numbers = new int[] { 1, 2, 3, 4 };
+                result =  numbers[position];
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex);
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+
+            return result;
+        }
+    }
+}
---
+++ exHandling/Program.cs
@@ -0,0 +1,53 @@
+﻿using ExceptionsLibrary;
+
+namespace exHandling;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("exHandling");
+        DemoCode demo = new();
+
+        /*try
+        {
+            int result = demo.GrandParent(5);
+            Console.WriteLine($"result at given position is {result}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }*/
+
+        /*int result = demo.GrandParent(4);
+        Console.WriteLine($"result at given position is {result}");*/
+        int num = Main1();
+        Console.WriteLine($"returning value {num}");
+    }
+    
+    public static int Main1()
+    {
+        int returnValue = 0;
+        try
+        {
+            returnValue += 1;
+            Console.WriteLine($"Doing smth inside try, opening connection {returnValue}");
+            throw new Exception();
+            return returnValue;
+
+        }
+        catch (Exception ex)
+        {
+            returnValue += 1;
+            Console.WriteLine($"Exception is cought {returnValue}");
+        }
+        finally
+        {
+            returnValue += 1;
+            Console.WriteLine($"Executing finally command {returnValue}");
+        }
+        Console.WriteLine(returnValue);
+        return returnValue;
+    }
+
+}
